dea4f8f38a54a3f8a6d42039a580d6d01bac7e49
vsyniohub <storm6594@gmail.com>
2020-04-01 22:12:17 +0200

    Added Pagination for Expenses, not working yet. Added Truncation for Subject in a list of Expenses. Saved some useful stuff in TrackerSample.

--- src/aura/ExpenseListWrapper/ExpenseListWrapperHelper.js
+++ src/aura/ExpenseListWrapper/ExpenseListWrapperHelper.js
@@ -3,7 +3,8 @@
         var idx = event.currentTarget.getAttribute("id");
     },
 	doInit : function(component, event) {
-		var action = component.get("c.getMyExpenses");
+        var action = component.get("c.getMyExpenses");
+        var endPositionDefault = 8;
         
         action.setCallback(this, function(response) {
             var state = response.getState();
@@ -12,22 +13,40 @@
                 if (expenses.length > 0) {
                     component.set("v.myExpenses", expenses);
                     component.set("v.areRecords", true);
+                    component.set("v.recordAmount", expenses.length);
+                    component.set("v.endPosition", endPositionDefault);
+                    this.cleaveOutputList(component);
                 }
-                
             } else if (state === "ERROR") {
-                var errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        console.log("Error message: " + errors[0].message);
-                    }
-                } else {
-                    console.log("Unknown error");
-                }
+                this.handleError(response.getError());
             }
         });
         $A.enqueueAction(action);
 	},
 	toggleNewButton : function(component, event) {
         component.set("v.showNew",!component.get("v.showNew"));
-	}
+    },
+    handleError : function(errors) {
+        if (errors) {
+            if (errors[0] && errors[0].message) {
+                console.log("Error message: " + errors[0].message);
+            }
+        } else {
+            console.log("Unknown error");
+        }
+    },
+    cleaveOutputList : function(component) {
+        var maxRows         = 8;
+        var myExpenses      = component.get("v.myExpenses");
+        var startPosition   = component.get("v.startPosition");
+        var endPosition     = component.get("v.endPosition");
+
+        if (myExpenses ) {
+            if (myExpenses.length > maxRows) {
+                component.set("v.myExpensesToShow", myExpenses.slice(startPosition, endPosition));
+            } else {
+                component.set("v.myExpensesToShow", myExpenses);
+            }
+        }
+    }
 })
