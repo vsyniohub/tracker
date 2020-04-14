b7ebe4964d07f9ed9dd80686a0093476b9c16352
vsyniohub <storm6594@gmail.com>
2020-04-14 20:41:12 +0200

    Added responsive for Browser and Mobile devices

--- src/aura/ExpenseListWrapper/ExpenseListWrapperController.js
+++ src/aura/ExpenseListWrapper/ExpenseListWrapperController.js
@@ -32,10 +32,10 @@
                         header: "Edit Expense",
                         body: modalBody,
                         showCloseButton: false,
-                        cssClass: "class",
+                        cssClass: "mymodal",
                         closeCallback: function() {
                             component.find("overlayLib").notifyClose();
-                            //$A.get('e.force:refreshView').fire();
+                            $A.get('e.force:refreshView').fire();
                         }
                    })
                 }
