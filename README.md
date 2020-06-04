644aad943e4d4beb57ee09c356dbb1a0a7c92711
vsyniohub <storm6594@gmail.com>
2020-06-04 21:45:19 +0200

    First Chart started working. Flickering on hover. Chart JS - correct static resource

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Pet-project to switch to Lighning Web Components.
 Not to forget:
 1. Settings List
 2. Modules List
+3. Color Picker
--- src/lwc/mainGraphContainer/mainGraphContainer.js
+++ src/lwc/mainGraphContainer/mainGraphContainer.js
@@ -5,49 +5,16 @@ import chartJSLibrary from '@salesforce/resourceUrl/ChartJS23';
 
 export default class MainGraphContainer extends LightningElement {
     @track filterName = 'YEAR';
+    @track chartLabel = '';
+    @track isData = true;
+    @track noDataCaption = '';
     @track chartData;
     @track dailyChartSelectedState = false;
-    @track weeklyChartSelectedState = true;
+    @track weeklyChartSelectedState = false;
     @track monthlyChartSelectedState = false;
+
+    localStorageFilterName = 'filterName';
     
-    //@wire(getAggregatedExpenses, { period: '$filterName' }) chartData;
-    /*
-    @wire(getAggregatedExpenses, { period: '$filterName' }) putChartData(dataRetrieved, error) {
-        if (typeof dataRetrieved !== 'undefined') {
-            this.chartData = JSON.stringify(dataRetrieved);
-            var labelset = [];
-            var dataset = [];
-
-            console.log(dataRetrieved);
-            console.log(document.getElementById("pie-chart"));
-            
-            dataRetrieved.data.forEach(function(key) {
-                labelset.push(key.label); 
-                dataset.push(key.count); 
-            });
-
-            new Chart(document.getElementById("pie-chart"), {
-                type: 'pie',
-                data: {
-                    labels:labelset,
-                    datasets: [{
-                        label: "Count of Task",
-                        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
-                        data: dataSet
-                    }]
-                },
-                options: {
-                    title: {
-                        display: true,
-                        text: 'Expenses This Week'
-                    }
-                }
-            });
-        } else {
-            this.chartData = JSON.stringify(error);
-        }
-    };*/
-
     renderedCallback() {
         Promise.all([
             loadScript(this, chartJSLibrary),
@@ -59,48 +26,80 @@ export default class MainGraphContainer extends LightningElement {
     }
 
     dailyChartSelected(event) {
+        this.filterName = 'DAY';
+        this.chartData = 'Expenses Today';
+        this.noDataCaption = 'Today';
 
+        this.resetAllButtons();
+        console.log(localStorage.getItem(localStorageFilterName));
+        this.dailyChartSelectedState = true;
+        this.retrieveDataAggregated();
     }
     weeklyChartSelected(event) {
-
+        this.filterName = 'WEEK';
+        this.chartData = 'Expenses This Week';
+        this.noDataCaption = 'This Week';
+        
+        this.resetAllButtons();
+        this.weeklyChartSelectedState = true;
+        this.retrieveDataAggregated();
+        localStorage.setItem(localStorageFilterName, this.filterName);
     }
     monthlyChartSelected(event) {
-        
+        this.filterName = 'MONTH';
+        this.chartData = 'Expenses This Month';
+        this.noDataCaption = 'This Month';
+
+        this.resetAllButtons();
+        this.monthlyChartSelected = true;
+        this.retrieveDataAggregated();
+        localStorage.setItem(localStorageFilterName, this.filterName);
     }
     retrieveDataAggregated() {
         getAggregatedExpenses({period: this.filterName})
         .then(dataRetrieved => {
             console.log(dataRetrieved);
-
-            var labelSet = [];
-            var dataSet = [];
-            
-            dataRetrieved.forEach(function(key) { 
-                labelSet.push(key.label); 
-                dataSet.push(key.sumAmount); 
-            });
-
-            let chartObject = this.template.querySelectorAll('canvas');
+            if (typeof dataRetrieved !== 'undefined' && dataRetrieved.length > 0) {
+                var labelSet = [];
+                var dataSet = [];
+                
+                dataRetrieved.forEach(function(key) { 
+                    labelSet.push(key.label); 
+                    dataSet.push(key.sumAmount); 
+                });
+                this.isData = true;
+                this.generateChart(dataSet, labelSet);
+            } else {
+                this.isData = false;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+    generateChart(dataSet, labelSet) {
+        let chartObject = this.template.querySelectorAll('canvas');
             new Chart(chartObject, {
                 type: 'pie',
                 data: {
                     labels:labelSet,
                     datasets: [{
                         label: "Spent",
-                        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
+                        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f", "#1155c2"],
                         data: dataSet
                     }]
                 },
                 options: {
                     title: {
                         display: true,
-                        text: 'Expenses This Week'
+                        text: this.chartLabel
                     }
                 }
             });
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    }
+    resetAllButtons() {
+        this.dailyChartSelectedState = false;
+        this.weeklyChartSelectedState = false;
+        this.monthlyChartSelectedState = false;
     }
 }
