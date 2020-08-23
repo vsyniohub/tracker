431b4baea5425c9542d4b6b36d4b9f673d0a7b0a
vsyniohub <storm6594@gmail.com>
2020-08-23 20:15:57 +0200

    Notes List Controller class added and Text Note Item component changes

--- src/lwc/textNoteItem/textNoteItem.js
+++ src/lwc/textNoteItem/textNoteItem.js
@@ -1,4 +1,5 @@
 import { LightningElement } from 'lwc';
+import getNotes from '@salesforce/apex/NotesListController.getMyNotes';
 
 export default class TextNoteItem extends LightningElement {
     /**
@@ -8,4 +9,16 @@ export default class TextNoteItem extends LightningElement {
         'bold', 'italic', 'underline', 'list', 'indent',
         'image', 'clean', 'table', 'header', 'color'
     ];
+
+    retrieveData() {
+        getNotes({
+        }).then(dataRetrieved => {
+            if (typeof dataRetrieved !== 'undefined' && dataRetrieved.length > 0) {
+                
+            } else {
+            }
+        }).catch(error => {
+            console.log(error);
+        });
+    }
 }
